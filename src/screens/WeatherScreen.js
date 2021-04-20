0ad9835dbb4155997a2b8d464828d8b19f86c955
iqbalfaisal <7faisaliqbal@gmail.com>
2021-04-20 14:48:41 +0500

    homeScreen UI

--- src/screens/WeatherScreen.js
+++ src/screens/WeatherScreen.js
@@ -1,5 +1,6 @@
 import * as React from 'react';
-import {View, Text} from 'react-native';
+import MapView from 'react-native-maps';
+import {View, Text, StyleSheet} from 'react-native';
 
 function WeatherScreen({
   navigation,
@@ -10,10 +11,67 @@ function WeatherScreen({
   console.log(data);
 
   return (
-    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <Text>Home Screen</Text>
-    </View>
+    <>
+      <MapView
+        region={{
+          latitude: data.coord.lat,
+          longitude: data.coord.lon,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+      />
+      <View
+        style={{
+          position: 'absolute',
+          bottom: 0,
+          backgroundColor: 'white',
+          width: '100%',
+          padding: 20,
+        }}>
+        <View
+          style={{
+            flexDirection: 'row',
+          }}>
+          <View
+            style={{
+              flexDirection: 'column',
+            }}>
+            <Text style={[styles.weather, {fontWeight: 'bold'}]}>
+              {data.name}
+            </Text>
+            <Text style={styles.weather}>{data.weather[0].description}</Text>
+
+            <Text style={styles.weather}>Humidity: {data.main.humidity}</Text>
+            <Text style={styles.weather}>Wind Speed: {data.wind.speed}</Text>
+            <Text style={styles.weather}>Min Temp: {data.main.temp_min}</Text>
+            <Text style={styles.weather}>Max Temp: {data.main.temp_max}</Text>
+          </View>
+
+          <View
+            style={{
+              flexDirection: 'column',
+              justifyContent: 'center',
+              alignContent: 'center',
+              alignItems: 'center',
+              alignSelf: 'center',
+              flex: 1,
+            }}>
+            <Text style={[styles.weather, {fontWeight: 'bold'}]}>
+              {data.main.temp}
+            </Text>
+          </View>
+        </View>
+      </View>
+    </>
   );
 }
+
+const styles = StyleSheet.create({
+  weather: {
+    fontSize: 14,
+    color: 'black',
+    paddingVertical: 5,
+  },
+});
 
 export default WeatherScreen;
